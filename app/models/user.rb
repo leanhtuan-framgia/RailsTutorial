c92f54f5de78813a699adf3aae899aad618c82cd
TuanLe <le.anh.tuan@framgia.com>
2016-07-19 14:40:08 +0700

    Chapter 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
   class User < ApplicationRecord
+    attr_accessor :remember_token
     before_save {email.downcase!}
 
     validates :name, presence: true, length: {maximum: 50}
@@ -15,10 +16,29 @@
       format: {with: VALID_PHONE_NUMBER_REGEX}
 
     class << self
-    def digest string
-      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                    BCrypt::Engine.cost
-      BCrypt::Password.create string, cost: cost
+      def digest string
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+          BCrypt::Engine.cost
+        BCrypt::Password.create string, cost: cost
+      end
+
+      def new_token
+        SecureRandom.urlsafe_base64
+      end
     end
+
+    def remember
+      self.remember_token = User.new_token
+      update_attributes remember_digest: User.digest(remember_token)
     end
+
+    def authenticated? remember_token
+      return false if remember_digest.nil?
+      BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+
+    def forget
+      update_attribute :remember_digest, nil
+    end
+
   end
