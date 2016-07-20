a1bef0f3e415524c4c8324927d4082bb90636466
TuanLe <le.anh.tuan@framgia.com>
2016-07-20 11:17:07 +0700

    Chap 10

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@
 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: {maximum: 255},
-      format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+      format: {with: VALID_EMAIL_REGEX},
+      uniqueness: {case_sensitive: false}
 
     has_secure_password
     validates :password, presence: true, length: {minimum: 6}
@@ -27,9 +28,13 @@
       end
     end
 
+    def correct_user? current_user
+      self == current_user
+    end
+
     def remember
       self.remember_token = User.new_token
-      update_attributes remember_digest: User.digest(remember_token)
+      update_attribute :remember_digest, User.digest(remember_token)
     end
 
     def authenticated? remember_token
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  User.create! name: "Example User",
+   email: "example@railstutorial.org",
+   password: "foobar",
+   password_confirmation: "foobar",
+   phone_number: "0111111000"
+
+  99.times do |n|
+    name  = Faker::Name.name
+    email = "example-#{n+1}@railstutorial.org"
+    password = "password"
+    phone_number = "011111011#{n+1}"
+    User.create! name: name,
+      email: email,
+      password: password,
+      password_confirmation: password,
+      phone_number: phone_number
+  end
