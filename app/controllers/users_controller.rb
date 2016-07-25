3479a1a0176208fd9c2a095bac915c6027029128
TuanLe <le.anh.tuan@framgia.com>
2016-07-25 13:47:32 +0700

    Chap 11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
+  before_action :logged_in_user, except: [:new, :show, :create]
   before_action :admin_user, only: :destroy
-  before_action :find_user, only: [:show, :edit, :update, :destroy]
+  before_action :find_user, except: [:new, :create, :index]
   before_action :correct_user, only: [:edit, :update]
 
   def index
-    @users = User.paginate page: params[:page]
+    @users = User.activated.paginate page: params[:page]
   end
 
   def show
@@ -18,8 +18,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      log_in @user
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email for activation"
+      redirect_to root_url
     else
       render :new
     end
@@ -38,7 +39,7 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find_by(id: params[:id]).destroy
+    @user.destroy
     flash[:sucess] = "Delete user successful"
     redirect_to users_url
   end
@@ -54,6 +55,6 @@ class UsersController < ApplicationController
   end
 
   def find_user
-     @user = User.find_by id: params[:id]
+    @user = User.find_by id: params[:id]
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
   class User < ApplicationRecord
-    attr_accessor :remember_token
-    before_save {email.downcase!}
+    attr_accessor :remember_token, :activation_token
+    before_save :email_downcase
+    before_create :create_activation_digest
 
     validates :name, presence: true, length: {maximum: 50}
 
@@ -8,13 +9,14 @@
     validates :email, presence: true, length: {maximum: 255},
       format: {with: VALID_EMAIL_REGEX},
       uniqueness: {case_sensitive: false}
-
     has_secure_password
     validates :password, presence: true, length: {minimum: 6}
 
     VALID_PHONE_NUMBER_REGEX = /0\d{9,10}/
     validates :phone_number, presence: true,
       format: {with: VALID_PHONE_NUMBER_REGEX}
+
+    scope :activated, ->{where("activated = ?", true)}
 
     class << self
       def digest string
@@ -37,13 +39,31 @@
       update_attribute :remember_digest, User.digest(remember_token)
     end
 
-    def authenticated? remember_token
-      return false if remember_digest.nil?
-      BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    def authenticated? attribute, token
+      digest = send "#{attribute}_digest"
+      return false if digest.nil?
+      BCrypt::Password.new(digest).is_password?(token)
     end
 
     def forget
       update_attribute :remember_digest, nil
     end
 
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest activation_token
+    end
+
+    def activate
+      update_columns activated: true, activated_at: Time.zone.now
+    end
+
+    def send_activation_email
+      UserMailer.account_activation(self).deliver_now
+    end
+
+    private
+    def email_downcase
+      self.email = email.downcase
+    end
   end
