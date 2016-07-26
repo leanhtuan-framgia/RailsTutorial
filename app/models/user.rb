dd610ab3d0bbdc3eb411e4855b49582dc9d2c85f
TuanLe <le.anh.tuan@framgia.com>
2016-07-26 10:06:52 +0700

    Chap 12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
   class User < ApplicationRecord
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
     before_save :email_downcase
     before_create :create_activation_digest
 
@@ -28,6 +28,16 @@
       def new_token
         SecureRandom.urlsafe_base64
       end
+    end
+
+    def create_reset_digest
+      self.reset_token = User.new_token
+      update_columns reset_digest: User.digest(reset_token),
+        reset_sent_at: Time.zone.now
+    end
+
+    def send_password_reset_email
+      UserMailer.password_reset(self).deliver_now
     end
 
     def correct_user? current_user
@@ -60,6 +70,10 @@
 
     def send_activation_email
       UserMailer.account_activation(self).deliver_now
+    end
+
+    def password_reset_expired?
+      reset_sent_at < 2.hours.ago
     end
 
     private
