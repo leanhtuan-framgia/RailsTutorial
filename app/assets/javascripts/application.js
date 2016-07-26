a425868b75ac77272c58670d3b9efc567351f845
TuanLe <le.anh.tuan@framgia.com>
2016-07-26 15:35:28 +0700

    Chap 13

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,17 +1,24 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap
-//= require turbolinks
-//= require_tree .
+
+ // This is a manifest file that'll be compiled into application.js, which will include all the files
+ // listed below.
+ //
+ // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+ // or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+ //
+ // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+ // compiled file. JavaScript code in this file should be added after the last require_* statement.
+ //
+ // Read Sprockets README (https://github.com/rails/sprockets#sprocketsdirectives) for details
+ // about supported directives.
+ //
+ //= require jquery
+ //= require jquery_ujs
+ //= require bootstrap
+ //= require turbolinks
+ //= require_tree
+$("#micropost_picture").bind("change", function(){
+   var size_in_megabytes = this.file[0].size/1024/1024;
+   if (size_in_megabytes > 5) {
+     alert("Maximum file size is 5MB. Please choose a smaller file.")
+   }
+ });
